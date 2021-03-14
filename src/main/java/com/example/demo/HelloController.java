e37e492f38296eef7f73f5c8564d24d18c7dcad0
mathjava81 <justinbucc@gmail.com>
2021-03-14 17:44:34 -0400

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloController {
