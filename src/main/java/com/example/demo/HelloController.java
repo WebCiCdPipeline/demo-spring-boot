28fcff6bf3da3bb5658c7208aa672987ecacfaab
mathjava81 <justinbucc@gmail.com>
2021-03-14 17:42:39 -0400

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-	@GetMapping("("/")
+	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
